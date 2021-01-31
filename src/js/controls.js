7868376dc07f6fa3e1e2f9bd757260d1dd922655
João Costa <jdpc557@gmail.com>
2021-01-31 14:02:57 +0000

    Capture events to avoid page scrolling

--- src/js/controls.js
+++ src/js/controls.js
@@ -12,26 +12,29 @@ var shootMarble = false;
 var marbleTimeout = 0;
 
 function baseKeyHandler(e) {
-    if (e.keyCode == up) {
-      upPressed = e.type == 'keydown';
+  if([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+    event.preventDefault();
+  }
+  if (e.keyCode == up) {
+    upPressed = e.type == 'keydown';
+  }
+  else if (e.keyCode == down) {
+    downPressed = e.type == 'keydown';
+  }
+  else if (e.keyCode == left) {
+    leftPressed = e.type == 'keydown';
+  }
+  else if (e.keyCode == right) {
+    rightPressed = e.type == 'keydown';
+  }
+  else if (e.keyCode == shoot) {
+    if (e.type == 'keydown' && marbleTimeout < 0) {
+      shootMarble = true;
     }
-    else if (e.keyCode == down) {
-      downPressed = e.type == 'keydown';
+    else if (e.type == 'keyup') {
+      shootMarble = false;
     }
-    else if (e.keyCode == left) {
-      leftPressed = e.type == 'keydown';
-    }
-    else if (e.keyCode == right) {
-      rightPressed = e.type == 'keydown';
-    }
-    else if (e.keyCode == shoot) {
-      if (e.type == 'keydown' && marbleTimeout < 0) {
-        shootMarble = true;
-      }
-      else if (e.type == 'keyup') {
-        shootMarble = false;
-      }
-    }
+  }
 }
 
 onkeyup = function(e) {
